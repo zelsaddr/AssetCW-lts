9f4ad3ba5372843f14ce6398d9dccb3461f83f48
zelsaddr <zeldin26@gmail.com>
2024-08-02 17:02:19 +0700

    add change

--- cw_asset_cms.sql
+++ cw_asset_cms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 31, 2024 at 12:58 PM
+-- Generation Time: Aug 02, 2024 at 12:00 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -20,6 +20,40 @@ SET time_zone = "+00:00";
 --
 -- Database: `cw_asset_cms`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aset`
+--
+
+CREATE TABLE `aset` (
+  `id` int(11) NOT NULL,
+  `nomor_invoice` varchar(255) DEFAULT NULL,
+  `kode_aset` varchar(100) NOT NULL,
+  `barang_id` int(11) NOT NULL,
+  `identitas_barang` int(11) DEFAULT NULL,
+  `tanggal_barang_datang` date NOT NULL,
+  `foto_barang_path` varchar(255) NOT NULL,
+  `pengguna_id` int(11) NOT NULL,
+  `satuan_id` int(11) NOT NULL,
+  `kondisi` varchar(255) NOT NULL,
+  `lokasi_aset_id` int(11) NOT NULL,
+  `nilai_perolehan` bigint(20) NOT NULL,
+  `keterangan` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `aset`
+--
+
+INSERT INTO `aset` (`id`, `nomor_invoice`, `kode_aset`, `barang_id`, `identitas_barang`, `tanggal_barang_datang`, `foto_barang_path`, `pengguna_id`, `satuan_id`, `kondisi`, `lokasi_aset_id`, `nilai_perolehan`, `keterangan`, `created_at`, `updated_at`) VALUES
+(8, NULL, 'CW-19001.27.04.2015', 9, NULL, '2015-04-27', 'assets/aset-berwujud/F8vCu7zFReoH7Ku8K459Kpj1JQzQo5X8QNs8idZ8.jpg', 6, 1, 'Baik', 7, 0, 'ok', '2024-08-02 01:33:13', '2024-08-02 01:33:13'),
+(9, NULL, 'CW-19002.27.04.2015', 10, NULL, '2015-04-27', 'assets/aset-berwujud/2CYiUWh5w8RzNOK1yjG6GljRFxFLujMOtbvKXZY7.jpg', 6, 1, 'Baik', 7, 0, 'ok', '2024-08-02 01:33:57', '2024-08-02 01:33:57'),
+(10, NULL, 'CW-19003.27.04.2015', 11, NULL, '2015-04-27', 'assets/aset-berwujud/bgNMzZFw6jvqNYlKeaTKS4YNTRrvNoz8AFkXdaqD.jpg', 6, 1, 'Baik', 7, 0, 'ok', '2024-08-02 01:35:07', '2024-08-02 01:35:07'),
+(11, NULL, 'CW-19004.14.06.2015', 14, NULL, '2015-06-14', 'assets/aset-berwujud/J8uZoXqlzUZVHPBmjE5Q8kKX2AA6nuAWhbtHqnLG.jpg', 7, 1, 'Baik', 7, 0, 'ok', '2024-08-02 01:38:24', '2024-08-02 01:38:24');
 
 -- --------------------------------------------------------
 
@@ -44,7 +78,12 @@ CREATE TABLE `barang` (
 --
 
 INSERT INTO `barang` (`id`, `kategori_id`, `nama_barang`, `merk_barang`, `tahun_perolehan`, `foto_tampak_depan_path`, `foto_tampak_samping_path`, `created_at`, `updated_at`) VALUES
-(6, 51, 'Toyota Avanza 2021', 'Toyota', 2024, 'public/foto_tampak_depan/b9xrnkRD5Z0f2UAjlVVfR3CrrsFyYnGI89ArzMcV.jpg', 'public/foto_tampak_samping/oaktabNXkE5S8363d8t87wNS2fZ8DAkXr8nKTQDe.jpg', '2024-07-30 02:18:04', '2024-07-30 02:18:04');
+(9, 51, 'TERIOS TX - ADVENTURE A/T - B 2320 SFI', 'Daihatsu', 2015, 'public/foto_tampak_depan/WIhugLpEgXnvwmFzm81ITmd91kWXa6nEdfKGLh14.jpg', 'public/foto_tampak_samping/jDnYEC7k6JNQ1EbqR5LZWNJ3n9UU9S7XpVEtFhTn.jpg', '2024-08-02 00:56:57', '2024-08-02 00:56:57'),
+(10, 51, 'TERIOS TX - ADVENTURE A/T - B 2313 SFI', 'Daihatsu', 2015, 'public/foto_tampak_depan/6KJb0dtG30jnA2zYD6P8IldaSg0V8bn45C4ocCky.jpg', 'public/foto_tampak_samping/aJXR7aWv7t7D1NAS2VnuHSLGE0LoUjfc3zaUdDGw.jpg', '2024-08-02 00:57:43', '2024-08-02 00:57:43'),
+(11, 51, 'LUXIO - B 2308 SFI', 'Daihatsu', 2015, 'public/foto_tampak_depan/uItHCFz3Vw87ILX1NGpLSxpX8MWKROYrkRPLlZ0P.jpg', 'public/foto_tampak_samping/SygoifNxRLuRJRLKIfVvaCGNXmC1V2SwpiZQKh0M.jpg', '2024-08-02 00:59:33', '2024-08-02 00:59:33'),
+(14, 51, 'TERIOS TX - ADVENTURE A/T - B 2752 SFN', 'Daihatsu', 2015, 'public/foto_tampak_depan/YNkfcldQwkLCPkZ08v0LWyMkCKKshLxow2ordCS4.jpg', 'public/foto_tampak_samping/yNacikXRhUEVQmJVFl3EVbyMsjWbNGdnmOJBeqqi.jpg', '2024-08-02 01:01:03', '2024-08-02 01:01:03'),
+(15, 51, 'Avanza 1.3 E MT  - B 3 NDA', 'Toyota', 2024, 'public/foto_tampak_depan/yolr4q6LQvqsuAMTNymgzDFawxpYbZCdllSB695T.jpg', 'public/foto_tampak_samping/ZOWtLSH96bP12b0pUHMwGk2XXEltNQxgMRyusz9h.jpg', '2024-08-02 01:03:01', '2024-08-02 01:08:16'),
+(16, 51, 'Toyota Avanza 1.3 E MT - B 2433 UZM', 'Toyota', 2024, 'public/foto_tampak_depan/HK9rz0v5Odu66y6Bf7zWlYKj53oSt8n5eDi995bA.jpg', 'public/foto_tampak_samping/aZYsY2JA2T1NZ4jHmPJGKRvmQ4o60ulxftO9O6YN.jpg', '2024-08-02 01:03:28', '2024-08-02 01:08:29');
 
 -- --------------------------------------------------------
 
@@ -139,11 +178,8 @@ CREATE TABLE `lokasi_aset` (
 --
 
 INSERT INTO `lokasi_aset` (`id`, `nama_lokasi`, `created_at`, `updated_at`) VALUES
-(1, 'Gudang A', '2024-07-18 03:56:21', '2024-07-18 03:56:21'),
-(2, 'Kantor Utama', '2024-07-18 03:56:21', '2024-07-18 03:56:21'),
-(3, 'Pabrik 1', '2024-07-18 03:56:21', '2024-07-18 03:56:21'),
-(4, 'Ruang Server', '2024-07-18 03:56:21', '2024-07-18 03:56:21'),
-(5, 'Kantor Cabang Jakarta', '2024-07-18 03:56:21', '2024-07-18 03:56:21');
+(6, 'Andara', '2024-07-31 20:05:13', '2024-07-31 20:05:13'),
+(7, 'Mandala', '2024-07-31 20:10:32', '2024-07-31 20:10:32');
 
 -- --------------------------------------------------------
 
@@ -167,9 +203,9 @@ CREATE TABLE `pengguna_aset` (
 INSERT INTO `pengguna_aset` (`id`, `nama_pengguna`, `jabatan`, `posisi_pengguna`, `created_at`, `updated_at`) VALUES
 (1, 'John Doe', 'Manager', 'IT Department', '2024-07-18 03:56:37', '2024-07-18 03:56:37'),
 (2, 'Jane Smith', 'Supervisor', 'Finance Department', '2024-07-18 03:56:37', '2024-07-18 03:56:37'),
-(3, 'Michael Brown', 'Engineer', 'Maintenance Department', '2024-07-18 03:56:37', '2024-07-18 03:56:37'),
 (4, 'Emily Davis', 'Staff', 'HR Department', '2024-07-18 03:56:37', '2024-07-18 03:56:37'),
-(5, 'David Wilson', 'Director', 'Operations Department', '2024-07-18 03:56:37', '2024-07-18 03:56:37');
+(6, 'Div. Umum', 'Pengelola', 'Lt. 3', '2024-08-02 01:16:40', '2024-08-02 01:16:40'),
+(7, 'Budianto', 'Senior Officer Spv P&P', 'Lt. 1', '2024-08-02 01:36:41', '2024-08-02 01:36:41');
 
 -- --------------------------------------------------------
 
@@ -199,6 +235,16 @@ INSERT INTO `satuan` (`id`, `nama_satuan`, `created_at`, `updated_at`) VALUES
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `aset`
+--
+ALTER TABLE `aset`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `barang_id` (`barang_id`),
+  ADD KEY `pengguna_id` (`pengguna_id`),
+  ADD KEY `satuan_id` (`satuan_id`),
+  ADD KEY `lokasi_aset_id` (`lokasi_aset_id`);
 
 --
 -- Indexes for table `barang`
@@ -236,10 +282,16 @@ ALTER TABLE `satuan`
 --
 
 --
+-- AUTO_INCREMENT for table `aset`
+--
+ALTER TABLE `aset`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `kategori`
@@ -251,13 +303,13 @@ ALTER TABLE `kategori`
 -- AUTO_INCREMENT for table `lokasi_aset`
 --
 ALTER TABLE `lokasi_aset`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `pengguna_aset`
 --
 ALTER TABLE `pengguna_aset`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `satuan`
@@ -268,6 +320,15 @@ ALTER TABLE `satuan`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `aset`
+--
+ALTER TABLE `aset`
+  ADD CONSTRAINT `aset_ibfk_1` FOREIGN KEY (`barang_id`) REFERENCES `barang` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `aset_ibfk_2` FOREIGN KEY (`pengguna_id`) REFERENCES `pengguna_aset` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `aset_ibfk_3` FOREIGN KEY (`satuan_id`) REFERENCES `satuan` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `aset_ibfk_4` FOREIGN KEY (`lokasi_aset_id`) REFERENCES `lokasi_aset` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `barang`
